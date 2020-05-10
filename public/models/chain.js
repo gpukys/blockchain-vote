0beefff609313d93123f71e1a75c47e78a8b49c5
Gerimantas <g.pukys@gmail.com>
2020-05-10 22:03:38 +0300

    push

--- public/models/chain.js
+++ public/models/chain.js
@@ -57,7 +57,7 @@ module.exports = class CryptoBlockchain{
     })
     .then((result)=>{
         function computeHash(block){
-          return SHA256(block.index + block.precedingHash + block.timestamp + JSON.stringify({pollId: block.pollId, choiceId: block.choiceId}).toString())
+          return SHA256(block.index + block.precedingHash + block.timestamp + JSON.stringify({pollId: block.pollId, choiceId: block.choiceId})).toString()
         }
         return new Promise(function(resolve, reject) {
           result.toArray(function(err, block) {
@@ -69,7 +69,7 @@ module.exports = class CryptoBlockchain{
               const precedingBlock = block[i - 1];
       
               if(currentBlock.hash !== computeHash(currentBlock)){
-                  reject(false);
+                reject(false);
               }
               if(currentBlock.precedingHash !== precedingBlock.hash) {
                 reject(false);
